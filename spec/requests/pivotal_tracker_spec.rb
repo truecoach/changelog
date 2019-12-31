c686a0628ec551c515163c3dc8399820200760cd
Adam Steel <adamgsteel@gmail.com>
2019-12-31 10:43:10 -0700

    [refactor] clarify that request spec is for accepted story

--- spec/requests/pivotal_tracker_spec.rb
+++ spec/requests/pivotal_tracker_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'POST /pivotal_tracker', :pt, :slack do
-  let(:params) { FB.build(:pivotal_tracker_webhook, :accepted) }
-
   let_env do
     ENV['PT_BASIC_NAME'] = 'test'
     ENV['PT_BASIC_PSW'] = 'test'
@@ -14,13 +12,21 @@ RSpec.describe 'POST /pivotal_tracker', :pt, :slack do
     post pivotal_tracker_path, params: params, headers: { 'HTTP_AUTHORIZATION' => authorization }
   end
 
-  it 'is successful' do
-    ping
+  context 'given a Pivotal Tracker story has been accepted' do
+    let(:params) { FB.build(:pivotal_tracker_webhook, :accepted) }
 
-    expect(response.status).to eq(201)
-  end
+    it 'is successful' do
+      ping
 
-  it 'sends a slack message' do
-    expect { ping }.to change { fired_slack_hooks.size }.by(1)
+      expect(response.status).to eq(201)
+    end
+
+    it 'sends a slack message that the story has deployed' do
+      expect { ping }.to change { fired_slack_hooks.size }.by(1)
+
+      msg = fired_slack_hooks.last
+
+      expect(msg).to match(/deployed/)
+    end
   end
 end
